eb7cf1830cc4374f18783f72f63a010fd78548e8
Brittany Bowman <britpetty@gmail.com>
2019-03-21 09:41:39 -0600

    sixth, all functions and buttons working, part 2 step 1. Start from bullet 5.

--- server/db/create_product.sql
+++ server/db/create_product.sql
@@ -2,3 +2,14 @@ INSERT INTO houses
 (name, address, city, state, zip)
 VALUES
 ($1, $2, $3, $4, $5)
+
+-- PURPOSE: Adding extra columns for part 2 step 1.
+-- Use SQLTabs to connect to your database. Alter the houses table, adding colums for the image, monthly mortgage amount, and desired rent(hint: save the SQL command for this to show your mentor). 
+-- ALTER TABLE houses
+-- ADD img TEXT;
+
+-- ALTER TABLE houses
+-- ADD mortgage INTEGER;
+
+-- ALTER TABLE houses
+-- ADD rent INTEGER;
